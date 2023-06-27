a009d6ed11e379974ad8d78aa95aeca4eecb8c5b
giovana.borges <gigi12345as17@gmail.com>
2023-06-27 20:36:05 -0300

    :feat add properties

--- src/main/java/com/spaceapi/models/AnimeModel.java
+++ src/main/java/com/spaceapi/models/AnimeModel.java
@@ -1,4 +1,18 @@
 package com.spaceapi.models;
 
-public class AnimeModel {
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.List;
+
+@Data
+@Document(collection = "animes")
+public class AnimeModel extends MidiaModel{
+    @Id
+    String id;
+    String studio;
+    Integer epQt;
+    Integer seasonQt;
+    List<UsuarioModel> usuarioId;
 }
--- src/main/java/com/spaceapi/models/UsuarioModel.java
+++ src/main/java/com/spaceapi/models/UsuarioModel.java
@@ -1,4 +1,14 @@
 package com.spaceapi.models;
 
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@Document(collection = "user")
 public class UsuarioModel {
+    @Id
+    String id;
+    String name;
+    String password;
 }
