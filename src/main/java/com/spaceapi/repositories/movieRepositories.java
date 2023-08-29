dfe518091fa9e5092ebdc950fe946084a520044a
giovana.borges <gigi12345as17@gmail.com>
2023-08-29 08:25:53 -0300

    :fix added missed anotation

--- src/main/java/com/spaceapi/repositories/movieRepositories.java
+++ src/main/java/com/spaceapi/repositories/movieRepositories.java
@@ -2,9 +2,11 @@ package com.spaceapi.repositories;
 
 import com.spaceapi.models.MovieModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface movieRepositories extends JpaRepository<MovieModel,Long> {
     public MovieModel save(MovieModel MovieModel);
 }
