d731ec8805de16c696e5ba9ca32ea3b6992cd91d
giovana.borges <gigi12345as17@gmail.com>
2023-08-29 08:24:11 -0300

    :fix changed some properties

--- src/main/java/com/spaceapi/models/AnimeModel.java
+++ src/main/java/com/spaceapi/models/AnimeModel.java
@@ -2,6 +2,7 @@ package com.spaceapi.models;
 
 import com.spaceapi.ENUMS.genreMovieEnum;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
@@ -15,18 +16,23 @@ public class AnimeModel  {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NotNull
     private String studio;
+    @Min(value = 0, message = "O valor deve ser no mínimo 0")
     private Integer epQt;
+    @Min(value = 0, message = "O valor deve ser no mínimo 0")
     private Integer seasonQt;
     @NotNull
     private String title;
     @NotNull
     private String image;
     @NotNull
+    private String coverImage;
+    @NotNull
     private String synopsis;
     @NotNull
     private genreMovieEnum genre;
-
+    @NotNull(message = "A data não pode ser nula")
     private Date release_date;
 
 }
