bf1fdd8c139632dddce83c928ed06f5f91534a2a
giovana.borges <gigi12345as17@gmail.com>
2023-09-21 20:04:32 -0300

    :feat added userDTO to request of post

--- src/main/java/com/spaceapi/controllers/userController.java
+++ src/main/java/com/spaceapi/controllers/userController.java
@@ -1,9 +1,7 @@
 package com.spaceapi.controllers;
 
-import com.spaceapi.DTOS.AnimeDTO;
 import com.spaceapi.DTOS.UserDTO;
 import com.spaceapi.models.UserModel;
-import com.spaceapi.services.AnimeServices;
 import com.spaceapi.services.UserServices;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -18,7 +16,7 @@ public class userController {
     private UserServices services;
 
     @PostMapping("/addUser")
-    public ResponseEntity createUser(@RequestBody UserModel user){
+    public ResponseEntity createUser(@RequestBody UserDTO user){
         ResponseEntity response = services.saveUser(user);
         return response;
     }
